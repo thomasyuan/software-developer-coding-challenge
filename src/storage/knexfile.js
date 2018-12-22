1945e0215310dbff24a84354c64cab7deb3ee29c
thomasyuan <thomas@cppcoder.net>
2018-12-21 22:28:20 -0500

    update knexfile production configuratoin to make it work with postgresql
    To use postgresql as database:
    1. run postgresql locally.
    `docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=password postgres:alpine`
    
    2. run app with NODE_ENV=production
    `NODE_ENV=production npm start`

--- src/storage/knexfile.js
+++ src/storage/knexfile.js
@@ -10,27 +10,11 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
   production: {
     client: "postgresql",
     connection: {
-      database: "my_db",
-      user: "username",
+      database: "postgres",
+      user: "postgres",
       password: "password"
     },
     pool: {
